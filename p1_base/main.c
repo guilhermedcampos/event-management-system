f9eccda5b019f744440fce184cf7cdea1987f142
Guilherme Campos <guilhermedcampos7@gmail.com>
2023-11-16 21:59:25 +0000

    .

--- p1_base/main.c
+++ p1_base/main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 
 #include "constants.h"
@@ -132,47 +133,89 @@ void parse_jobs_file(int fd, const char *base_name) {
   }
 }
 
-  void process_jobs_directory() {
-      DIR *dir = opendir(JOBS_DIR);
+void process_jobs_directory(int max_proc) {
+    DIR *dir = opendir(JOBS_DIR);
 
-      if (dir == NULL) {
-          perror("Error opening directory");
-          return;
-      }
+    if (dir == NULL) {
+        perror("Error opening directory");
+        return;
+    }
 
-      struct dirent *entry;
-      while ((entry = readdir(dir)) != NULL) {
-              // Check if the file has a ".jobs" extension
-              if (endsWith(entry->d_name, ".jobs")) {
-                  printf("Found .jobs file: %s\n", entry->d_name);
-                  // Construct the full path to the job file
-                  char file_path[PATH_MAX];
-                  snprintf(file_path, sizeof(file_path), "%s/%s", JOBS_DIR, entry->d_name);
+    struct dirent *entry;
+    int active_processes = 0; // Counter for active child processes
 
-                char base_name[PATH_MAX];
-                snprintf(base_name, sizeof(base_name), "%.*s", (int)(strrchr(entry->d_name, '.') - entry->d_name), entry->d_name);
-                  
+    while ((entry = readdir(dir)) != NULL) {
+        // Check if the file has a ".jobs" extension
+        if (endsWith(entry->d_name, ".jobs")) {
+            printf("Found .jobs file: %s\n", entry->d_name);
 
-                  // Open the job file
-                  int fd = open(file_path, O_RDONLY);
-                  if (fd == -1) {
-                      perror("Error opening job file");
-                      continue;  // Move on to the next file
-                  }
+            // Construct the full path to the job file
+            char file_path[PATH_MAX];
+            snprintf(file_path, sizeof(file_path), "%s/%s", JOBS_DIR, entry->d_name);
 
-                  parse_jobs_file(fd, base_name);
-                  
-                  // Close the file descriptor when done
-                  close(fd);
-              }
-      } 
+            char base_name[PATH_MAX];
+            snprintf(base_name, sizeof(base_name), "%.*s", (int)(strrchr(entry->d_name, '.') - entry->d_name), entry->d_name);
 
-      closedir(dir);
-  }
+            // Open the job file
+            int fd = open(file_path, O_RDONLY);
+            if (fd == -1) {
+                perror("Error opening job file");
+                continue;  // Move on to the next file
+            }
+
+            // New child process
+            pid_t pid = fork();
+
+            if (pid == 0) {
+                // Child process
+                parse_jobs_file(fd, base_name);
+                close(fd);
+                exit(0);
+            } else if (pid > 0) {
+                // Parent process
+                active_processes++;
+
+                // Check if the maximum number of processes has been reached
+                while (active_processes >= max_proc) {
+                    int status;
+                    pid_t child_pid = waitpid(-1, &status, WNOHANG);
+                    if (child_pid > 0) {
+                        active_processes--;
+                    }
+                }
+                close(fd);
+            } else {
+                // Fork failed
+                perror("Fork failed");
+            }
+        }
+    }
+
+    // Wait for remaining child processes to finish
+    while (active_processes > 0) {
+        int status;
+        pid_t child_pid = waitpid(-1, &status, 0);
+        if (child_pid > 0) {
+            active_processes--;
+        }
+    }
+
+    closedir(dir);
+}
+
 
 
 int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
+  int max_proc;
+
+  printf("Enter the maximum number of processes (MAX_PROC): ");
+  scanf("%d", &max_proc);
+
+  if (max_proc <= 0) {
+    fprintf(stderr, "Invalid value for MAX_PROC\n");
+    return 1;
+  }
 
   if (argc > 1) {
     char *endptr;
@@ -191,7 +234,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  process_jobs_directory();
+  process_jobs_directory(max_proc);
 
   while (1) {
     unsigned int event_id, delay;
