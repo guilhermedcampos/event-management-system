52ec0f0bf6c245d0316ab8a2d4d3d15030b6170a
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-12-10 22:25:51 +0000

    .

--- ex3/main.c
+++ ex3/main.c
@@ -167,7 +167,6 @@ void* process_file_thread(void *arg) {
 
 // Opens the jobs directory containing .jobs and .out
 void process_directory(char argv[], int max_threads) {
-
     pthread_t threads[max_threads];
 
     DIR *dir = opendir(argv);
@@ -179,73 +178,65 @@ void process_directory(char argv[], int max_threads) {
 
     struct dirent *entry;
 
-    // For each file found in the directory
     while ((entry = readdir(dir)) != NULL) {
-        // Check if the file has a ".jobs" extension
         if (endsWith(entry->d_name, ".jobs")) {
             printf("Found .jobs file: %s\n", entry->d_name);
 
-            // Reset the event list before processing each file
             reset_event_list();
 
-            // Construct the path to the job file
-            char file_path[PATH_MAX];
-            snprintf(file_path, sizeof(file_path), "%s/%s", argv, entry->d_name);
+            char *file_path = malloc(strlen(argv) + strlen(entry->d_name) + 1);
+            if (file_path == NULL) {
+                perror("Error allocating memory for file_path");
+                continue;
+            }
 
-            // Construct the file name
+            snprintf(file_path, strlen(argv) + strlen(entry->d_name) + 1, "%s/%s", argv, entry->d_name);
+
             char base_name[PATH_MAX];
             snprintf(base_name, sizeof(base_name), "%.*s", (int)(strrchr(entry->d_name, '.') - entry->d_name), entry->d_name);
 
-            // Open the job file
             int fd = open(file_path, O_RDONLY);
             if (fd == -1) {
                 perror("Error opening job file");
-                continue;  // Move on to the next file
+                free(file_path);
+                continue;
             }
 
-            // Create thread data and populate it
             for (int i = 0; i < max_threads; ++i) {
-                // Allocate separate memory for each thread
                 struct ThreadData *thread_data = (struct ThreadData *)malloc(sizeof(struct ThreadData));
                 thread_data->fd = fd;
                 snprintf(thread_data->base_name, sizeof(thread_data->base_name), "%s", base_name);
                 snprintf(thread_data->argv, sizeof(thread_data->argv), "%s", argv);
 
-                // Create threads to process the file
                 if (pthread_create(&threads[i], NULL, process_file_thread, (void *)thread_data) != 0) {
                     perror("Error creating thread");
+                    free(file_path);
                     return;
                 }
             }
 
-            // Wait for all threads to finish
             for (int i = 0; i < max_threads; ++i) {
                 pthread_join(threads[i], NULL);
             }
+
+            free(file_path);
         }
     }
 
-    // Close the jobs directory
     closedir(dir);
 }
 
 int main(int argc, char *argv[]) {
     unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
 
-
-    // Check if the number of arguments is correct
     if (argc != 2 && argc != 3) {
         fprintf(stderr, "Usage: %s <directory> [number]\n", argv[0]);
         return 1;
     }
 
-    // Set the directory
     char *directory = argv[1];
+    int max_threds = 1;
 
-    // Declare max_proc outside the if block
-    int max_threds = 1;  // Initialize to a default value, or any suitable default
-
-    // Check if the optional number argument is provided
     if (argc == 3) {
         char *endptr;
         max_threds = (int)strtoul(argv[2], &endptr, 10);
@@ -255,7 +246,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Failed to initialize EMS\n");
         return 1;
     }
-    // Process the directory
+
     process_directory(directory, max_threds);
 
     ems_terminate();
